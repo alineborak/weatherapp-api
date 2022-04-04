daed639b1783a9fd1ce50114fbfb4dc7793d4d24
Aline <36476341+alineborak@users.noreply.github.com>
2022-04-04 19:25:26 +0200

    weather app weather app working

--- src/index.js
+++ src/index.js
@@ -1,4 +1,64 @@
-// Homework week 4 - Part 1
+let apiKey = "384781b633046620eaed677419a0ac6e";
+
+function showTemperature(response) {
+  // console.log(response.data.name);
+  let temp = Math.round(response.data.main.temp);
+  let temperatureResult = document.querySelector("#temperature");
+  temperatureResult.innerHTML = `${temp}`;
+
+  let cityResult = document.querySelector("#city-display");
+  cityResult.innerHTML = response.data.name;
+
+  let precipitation = document.querySelector("#precipitation");
+  console.log(response.data);
+  // precipitation.innerHTML =
+}
+
+function search(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&appid=${apiKey}&units=metric`;
+  axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemperature);
+}
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", search);
+
+function currentTemp(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(function (position) {
+    let lat = position.coords.latitude;
+    let long = position.coords.longitude;
+    let units = "metric";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&units=${units}`;
+
+    axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemperature);
+    console.log(showTemperature);
+  });
+}
+
+let currentButton = document.querySelector("#current-location-button");
+currentButton.addEventListener("click", currentTemp);
+
+// Celsius/Farenheit
+
+function changeFarenheit(event) {
+  event.preventDefault();
+  let farenheit = document.querySelector("#temperature");
+  farenheit.innerHTML = 66;
+}
+function changeCelsius(event) {
+  event.preventDefault();
+  let celsius = document.querySelector("#temperature");
+  // celsius.innerHTML = `${currentTemp}°`;
+}
+
+let farenheitTemp = document.querySelector("#farenheit-link");
+let celsiusTemp = document.querySelector("#celsius-link");
+
+farenheitTemp.addEventListener(`click`, changeFarenheit);
+celsiusTemp.addEventListener(`click`, changeCelsius);
+// ------------------------------------------
 let now = new Date();
 console.log(now.getDate());
 
@@ -22,33 +82,3 @@ console.log(minutes);
 
 let time = document.querySelector("#day-time");
 time.innerHTML = `${day} ${hour}:${minutes}`;
-
-// Part 2
-function search(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector("#search-form-input");
-  let searchEng = document.querySelector("#city-display");
-  searchEng.innerHTML = `${searchInput.value}`;
-}
-let form = document.querySelector("#search-form");
-form.addEventListener("submit", search);
-
-// Part 3
-// #celsius-link #farenheit-link #temperature
-
-function changeFarenheit(event) {
-  event.preventDefault();
-  let farenheit = document.querySelector("#temperature");
-  farenheit.innerHTML = 66;
-}
-function changeCelsius(event) {
-  event.preventDefault();
-  let celsius = document.querySelector("#temperature");
-  celsius.innerHTML = 19;
-}
-
-let farenheitTemp = document.querySelector("#farenheit-link");
-let celsiusTemp = document.querySelector("#celsius-link");
-
-farenheitTemp.addEventListener(`click`, changeFarenheit);
-celsiusTemp.addEventListener(`click`, changeCelsius);
